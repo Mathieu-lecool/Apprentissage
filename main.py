72a69369a61535ffbc18a85b97af6288f7994d63
Mathieu-lecool <98987741+Mathieu-lecool@users.noreply.github.com>
2022-09-22 09:14:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from sense_emu import SenseHat
+
+sense = SenseHat()
+
+blue_color = [27, 172, 241]
+orange_color = [241, 69, 27]
+red_color = [255, 0, 0]
+
+while True:
+    temperature = int(sense.get_temperature())
+
+    if temperature < 25:
+        matrice_color = [blue_color] * 64
+    elif temperature >= 25 and temperature < 30:
+        matrice_color = [orange_color] * 64
+    else:
+        matrice_color = [red_color] * 64
+
+    sense.set_pixels(matrice_color)
